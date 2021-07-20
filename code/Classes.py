79b326b880e2f4a251b14e93d7921dc8d6ef3bac
Jan iMac <esel573@gmail.com>
2021-07-20 10:30:57 +0200

    klasse

--- code/Classes.py
+++ code/Classes.py
@@ -1,20 +1,22 @@
 class Car:
-    def __init__(self, name, year, hp): # self (this in C++/Java)
+    def __init__(self, name, year, ps): # self (this in C++/Java)
         self.name = name
         self.year = year
-        self.hp = hp
+        self.ps = ps
 
     def print_car(self):
-        print("The name of my car is: ", self.name)
+        print("Car: ", self.name)
 
-    def print_hp(self):
-        print("The car has", self.hp, " hp's.")
+    def print_ps(self):
+        print("Leistung: ", self.ps, " PS")
 
 
 my_car = Car("Audi A1 Sportback 40 TFSI", "2020", "200")
 my_car.print_car()
-my_car.print_hp()
+my_car.print_ps()
 
 my_car2 = Car("Opel Insignia", "2020", "120")
 my_car2.print_car()
-my_car2.print_hp()
+my_car2.print_ps()
+
+print("")
